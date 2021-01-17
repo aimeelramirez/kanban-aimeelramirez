a390c875a53978cb8edefb0202d8201fad01a70a
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-01-17 16:50:04 -0500

    committing refactoring and modal updates

--- js/data.js
+++ js/data.js
@@ -1,21 +1,32 @@
 // read data
+
+const utilSort = (dataRead) => {
+  dataRead.items.sort(function (a, b) {
+    return a.id - b.id
+  })
+  //console.log("check:" + JSON.stringify(dataRead.items))
+}
 const getBackLog = (queryTasks, dataRead) => {
-  console.log(queryTasks.id + ":" + dataRead.items)
-  // let getId = document.getElementById("backlog")
-  //   let getBackLog = document.getElementById("backlog");
+  // console.log(queryTasks.id + ":" + dataRead.items)
   let backlogTasks = queryTasks.querySelectorAll("article")
   let getLastArticle = backlogTasks[backlogTasks.length - 1]
   getLastArticle.remove()
   for (let i = 0; i < dataRead.items.length; i++) {
     //TODO sort the ids
+    utilSort(dataRead)
+    // console.log("check:" + JSON.stringify(dataRead.items))
+
     queryTasks.innerHTML +=
-      ` <article><h3>` +
+      ` <article>` +
+      dataRead.items[i].id +
+      `)` +
+      `<h3>` +
       dataRead.items[i].title +
-      `</h3>
-          <p>` +
+      `</h3>` +
+      `<p>` +
       dataRead.items[i].description +
-      `</p>
-          <p>
+      `</p>` +
+      `<p>
             <input
               type="date"
               id="date-created"
@@ -30,20 +41,21 @@ const getBackLog = (queryTasks, dataRead) => {
 const getImplementation = (queryTasks, dataRead) => {
   //console.log(queryTasks)
   let implementationTasks = queryTasks.querySelectorAll("article")
-
   let getLastArticle = implementationTasks[implementationTasks.length - 1]
   getLastArticle.remove()
-
   for (let i = 0; i < dataRead.items.length; i++) {
-    //TODO sort ids
+    utilSort(dataRead)
     queryTasks.innerHTML +=
-      ` <article><h3>` +
+      ` <article>` +
+      dataRead.items[i].id +
+      `)` +
+      `<h3>` +
       dataRead.items[i].title +
-      `</h3>
-          <p>` +
+      `</h3>` +
+      `<p>` +
       dataRead.items[i].description +
-      `</p>
-          <p>
+      `</p>` +
+      `<p>
             <input
               type="date"
               id="date-created"
@@ -58,30 +70,20 @@ const getComplete = (queryTasks, dataRead) => {
   let completeTasks = queryTasks.querySelectorAll("article")
   let getLastArticle = completeTasks[completeTasks.length - 1]
   getLastArticle.remove()
-  //TODO sort ids
-  //   let sortData = []
-  //   for (let key in dataRead.items) {
-  //     sortData.push({
-  //       id: dataRead.items[key].id,
-  //       title: dataRead.items[key].title,
-  //       description: dataRead.items[key].description,
-  //     })
-  //   }
-
   for (let i = 0; i < dataRead.items.length; i++) {
-    // dataRead.items.sort(function (a, b) {
-    //   return a.id - b.id
-    // })
+    utilSort(dataRead)
     // console.log("check:" + JSON.stringify(dataRead.items))
-
     queryTasks.innerHTML +=
-      ` <article><h3>` +
+      ` <article>` +
+      dataRead.items[i].id +
+      `)` +
+      `<h3>` +
       dataRead.items[i].title +
-      `</h3>
-          <p>` +
+      `</h3>` +
+      `<p>` +
       dataRead.items[i].description +
-      `</p>
-          <p>
+      `</p>` +
+      `<p>
             <input
               type="date"
               id="date-created"
