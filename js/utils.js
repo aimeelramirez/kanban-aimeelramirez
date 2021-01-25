975bf46a82ca7243ea1ce59b68e073c89a5b196e
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-01-25 00:01:04 -0500

    adding aria labels, meta,  and title

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,27 @@
+let newLabel = document.querySelectorAll("section")
+
 const ariaLabelHeaders = () => {
-  console.log("aria labels")
-  //role="list"
-  //let body = document.getElementsByTagName("body")
-  let newLabel = document.querySelectorAll("section")
+  // console.log("aria labels")
   let newLabelList = document.getElementsByTagName("article")
   for (let i = 0; i < newLabel.length; i++) {
-    newLabel[i].setAttribute("role", "list")
+    if (i < 3) {
+      newLabel[i].setAttribute("role", "list")
+    }
   }
   for (let i = 0; i < newLabelList.length; i++) {
     newLabelList[i].setAttribute("role", "listitem")
   }
-  console.log(newLabel)
 }
+const ariaLabelButtons = () => {
+  let buttonLabel = document.querySelectorAll("button")
+  for (let i = 0; i < buttonLabel.length; i++) {
+    let getTextButton = buttonLabel[i].querySelector("strong").innerText
+    let getListDescription = newLabel[i].id
+    console.log("button: ", getTextButton)
+    buttonLabel[i].setAttribute("aria-label", getListDescription)
+    buttonLabel[i].setAttribute("aria-describedby", getTextButton)
+  }
+}
+
 ariaLabelHeaders()
+ariaLabelButtons()
